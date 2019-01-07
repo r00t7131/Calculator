dab3f7e0bba501634f8922b5af1c588c56d6595b
Manik Garg <40022498+gmanik0001@users.noreply.github.com>
2019-01-07 22:30:10 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,23 @@
+output = []
 m = 0
 while m <= 0:
     x = float(input("Enter First Number: "))
     y = float(input("Enter Second Number: "))
     z = raw_input("Enter The Operator(+,-,*,/): ")
     if z == "+":
-        print x + y
+        print "result: " + str(x + y)
+        output.append(str(x + y))
     elif z == "-":
-        print x - y
+        print "result: " + str(x - y)
+        output.append(str(x - y))
     elif z == "*":
-        print x * y
+        print "result: " + str(x * y)
+        output.append(str(x * y))
     elif z == "/":
-        print x / y
+        print "result: " + str(x / y)
+        output.append(str(x / y))
     k = raw_input("\nPress Enter to continue or Press 'n' to exit: ")
     if k == "n":
         break
-    print" "
+    print"\n"
+print "Results: " + ", ".join(output)
